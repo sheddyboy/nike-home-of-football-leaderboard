bea46b5f9f2493c6f69341de4119a5349db50dcb
personal <shedrackitulah@gmail.com>
2024-06-10 20:12:42 +0100

    fix: increase link touch area

--- src/components/NavBtn.tsx
+++ src/components/NavBtn.tsx
@@ -14,9 +14,14 @@ export default function NavBtn({ text, link, active, onClick }: NavBtnProps) {
       onClick={onClick}
       className={`${
         !active && "pointer-events-none opacity-20"
-      } border-t-[1.5px] font-helvetica max-w-[202px] max-sm:max-w-[105px] w-[50%] font-bold text-[20.825px] max-sm:text-[12px] leading-[95%] tracking-[0.208px] max-sm:tracking-[0.12px] border-t-black pt-[25px] max-sm:pt-[10px] text-center`}
+      } border-t-[1.5px] font-helvetica max-w-[202px] max-sm:max-w-[105px] w-[50%] font-bold text-[20.825px] max-sm:text-[12px] leading-[95%] tracking-[0.208px] max-sm:tracking-[0.12px] border-t-black text-center`}
     >
-      <Link href={link}>{text}</Link>
+      <Link
+        href={link}
+        className="flex justify-center items-center pt-[25px] max-sm:pt-[10px]"
+      >
+        {text}
+      </Link>
     </div>
   );
 }
